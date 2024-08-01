dae9af8a45bb9b792632e951f8dc45e1a04df548
Florian Chaudret <florian.chaudret@mobility-work.com>
2024-08-01 23:53:24 +0200

    front: message sent confirmed

--- front/app/src/form-contact.ts
+++ front/app/src/form-contact.ts
@@ -16,6 +16,9 @@ export class FormContact extends LitElement {
     isLoading: boolean;
 
     @state()
+    sent: boolean
+
+    @state()
     showErrorSubmit: boolean;
 
     @state()
@@ -27,39 +30,48 @@ export class FormContact extends LitElement {
         super();
         this.sdk = new Sdk('http://localhost:8080');
         this.dto = NewMessageDto.fromEmpty();
+        this.sent = false;
         this.isLoading = false;
         this.isDisabled = true;
         this.showErrorSubmit = false;
         this.errorsForm = null;
     }
 
+    renderForm(): unknown
+    {
+        return html`
+            <form @submit="${this._submitForm}">
+                ${this.showErrorSubmit ? html`<fg-alert @close-alert="${this._handleCloseAlert}">Erreur pendant l'envoie du formulaire</fg-alert>`: html``}
+                <div>
+                    <label for="name">Nom</label>
+                    ${this.errorsForm?.name ? html`<div class="error">${this.errorsForm.name}</div>`: html`` }
+                    <wired-input placeholder="Enter name" name="name" .value="${this.dto.name}" @input="${this._handleInput}"></wired-input>    
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    ${this.errorsForm?.email ? html`<div class="error">${this.errorsForm.email}</div>`: html`` }
+                    <wired-input placeholder="Enter email" type="text" name="email" .value="${this.dto.email}" @input="${this._handleInput}"></wired-input>    
+                </div>
+                <div>
+                    <label for="message">Message</label>
+                    ${this.errorsForm?.message ? html`<div class="error">${this.errorsForm.message}</div>`: html`` }
+                    <wired-textarea placeholder="Enter message" name="message" rows="6" .value="${this.dto.message}" @input="${this._handleMessage}"></wired-textarea>    
+                </div>
+                <div class="actions">
+                    <wired-button elevation="2" ?disabled=${this.isDisabled} @click="${this._submitForm}">
+                    ${this.isLoading ? html`Ca envoie ;)` : html `Envoyer`}
+                    </wired-button>
+                </div>
+            </form>
+        `
+    }
+
     render(): unknown {
         return html `
             <wired-card elevation="5">
                 <h4>Contact</h4>
-                <form @submit="${this._submitForm}">
-                    ${this.showErrorSubmit ? html`<fg-alert @close-alert="${this._handleCloseAlert}">Erreur pendant l'envoie du formulaire</fg-alert>`: html``}
-                    <div>
-                        <label for="name">Nom</label>
-                        ${this.errorsForm?.name ? html`<div class="error">${this.errorsForm.name}</div>`: html`` }
-                        <wired-input placeholder="Enter name" name="name" .value="${this.dto.name}" @input="${this._handleInput}"></wired-input>    
-                    </div>
-                    <div>
-                        <label for="email">Email</label>
-                        ${this.errorsForm?.email ? html`<div class="error">${this.errorsForm.email}</div>`: html`` }
-                        <wired-input placeholder="Enter email" type="text" name="email" .value="${this.dto.email}" @input="${this._handleInput}"></wired-input>    
-                    </div>
-                    <div>
-                        <label for="message">Message</label>
-                        ${this.errorsForm?.message ? html`<div class="error">${this.errorsForm.message}</div>`: html`` }
-                        <wired-textarea placeholder="Enter message" name="message" rows="6" .value="${this.dto.message}" @input="${this._handleMessage}"></wired-textarea>    
-                    </div>
-                    <div class="actions">
-                        <wired-button elevation="2" ?disabled=${this.isDisabled} @click="${this._submitForm}">
-                        ${this.isLoading ? html`Ca envoie ;)` : html `Envoyer`}
-                        </wired-button>
-                    </div>
-                </form>
+                ${this.sent ? html`<div>Message envoyé. <wired-link @click="${this._handleRenewForm()}">Envoyé à nouveau</wired-link></div>` : this.renderForm()}
+                
             </wired-card>
         `
     }
@@ -103,6 +115,7 @@ export class FormContact extends LitElement {
 
         this.isLoading = false;
         this.isDisabled = false;
+        this.sent = true;
     }
 
     _handleInput(event: Event) {
@@ -130,6 +143,11 @@ export class FormContact extends LitElement {
 
     _handleCloseAlert() {
         this.showErrorSubmit = false;
+    }
+
+    _handleRenewForm()
+    {
+        this.sent = false;
     }
 
     static styles = [css`
